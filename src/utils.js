d950ffcc8fc776a6a3faf8364b70359501221549
Forest Park <foresthpark@gmail.com>
2020-08-31 21:52:46 -0700

    secret request and Send secret to email done

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD,
+    },
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "forest@forestpark.dev",
+    to: address,
+    subject: "Login secret for prismagram 🔐",
+    html: `Hello! You login secret is <strong>${secret}</strong>.<br/>Copy it to the app to login.`,
+  };
+
+  return sendMail(email);
+};
